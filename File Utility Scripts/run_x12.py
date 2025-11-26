b873340561cf98aa95411ef5c1772fe2d1774887
Joseph Mckenna <87082922+JMC1027@users.noreply.github.com>
2025-11-25 21:04:30 -0500

    Add files via upload

---
+++ File Utility Scripts/run_x12.py
@@ -0,0 +1,23 @@
+import json
+import csv
+import traceback
+#from generateDentalX12_Batch import generate_dental_x12_batch
+from generateDentalX12_Single import generate_dental_x12_single
+
+
+def generate():
+    try:
+        json_file_path = "Output/JSON_Output/mockedDentalClaim.json"
+        generate_dental_x12_single(json_file_path)
+
+        # json_file_path = "Output/JSON_Output/mockedDentalClaim.json"
+        # generate_dental_x12_single(json_file_path)
+
+
+    except Exception as e:
+        print(f"Failed to generate X12 file: {e}")
+        traceback.print_exc()
+
+generate()
+
+    
